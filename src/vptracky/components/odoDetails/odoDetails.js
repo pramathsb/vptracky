d2fb2093a525429c7656605f496a91b7cb169691
Pramath S.B <pramathsb@gmail.com>
2021-09-09 10:30:37 +0530

    Code refactor

--- src/vptracky/components/odoDetails/odoDetails.js
+++ src/vptracky/components/odoDetails/odoDetails.js
@@ -1,47 +1,44 @@
+import { useState } from "react";
+
 function OdoDetails(props) {
   const headings = ["Date", "ODO Reading", "Fuel Price in Rs.", "Qty in Rs.", "Qty in Ltrs.", "Distance", "Mileage"];
 
-  const renderColumnDistance = (table, index) => {
-    const currOdo = table[index].odo;
-    const prevOdo = table[index - 1]?.odo;
-    return <>{index === 0 ? "-" : currOdo - prevOdo}</>;
-  };
+  const newTable = {};
+  const [state, setState] = useState({});
 
-  const renderColumnMileage = (table, index) => {
-    const currOdo = table[index].odo;
-    const prevOdo = table[index - 1]?.odo;
-    const currFuelConsumed = table[index].quantityLtrs;
-    const currFullTank = table[index]?.fullTank;
-    const prevFullTank = table[index - 1]?.fullTank;
-    return <>{index === 0 ? "-" : currFullTank && prevFullTank ? ((currOdo - prevOdo) / currFuelConsumed).toFixed(2) : "-"}</>;
-  };
+  newTable.head = ["Date", "ODO Reading", "Fuel Price in Rs.", "Qty in Rs.", "Qty in Ltrs.", "Distance", "Mileage"];
+  newTable.body = props.data.map((row, index) => {
+    const currOdo = row.odo;
+    const prev = props.data[index - 1];
+    const prevOdo = prev?.odo;
+    const currFuelConsumed = row.quantityLtrs;
+    const currFullTank = row.fullTank;
+    const prevFullTank = prev?.fullTank;
 
-  const renderColumns = (table, index, rKey) => {
-    return (
-        [<td data-key={rKey++} key={`index${rKey++}`}>
-          {renderColumnDistance(table, index)}
-        </td>,
-        <td data-key={rKey++} key={`index${rKey++}`}>
-          {renderColumnMileage(table, index)}
-        </td>]
-    );
-  };
+    const distance = index === 0 ? "-" : currOdo - prevOdo;
+    const mileage = index === 0 ? "-" : currFullTank && prevFullTank ? (distance / currFuelConsumed).toFixed(2) : "-";
 
-  const renderTable = (table) => {
-    let rKey = 0;
-    return table.map((row, index) => {
+    return { ...row, distance, mileage };
+  });
+
+  const renderHeading = (head) => {
+    return head.map((heading, index) => {
       return (
-        <tr data-key={rKey++} key={`index${rKey++}`}>
-          {Object.entries(row).map(([key, value], index) => {
-            if (key === "id" || key === "fullTank") return false;
-            return (
-                <td data-key={rKey++} key={`index${rKey++}`}>
-                  {value}
-                </td>
-            );
-          })}
+        <th scope="col" key={heading}>
+          {heading}
+        </th>
+      );
+    });
+  };
 
-          {renderColumns(table, index, rKey)}
+  const renderBody = (body) => {
+    return body.map((col, index) => {
+      return (
+        <tr>
+          {Object.entries(col).map(([key, value], index) => {
+            if (key === "id" || key === "fullTank") return false;
+            return <td>{value}</td>;
+          })}
         </tr>
       );
     });
@@ -49,21 +46,13 @@ function OdoDetails(props) {
 
   return (
     <div>
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          {headings.map((heading, index) => {
-            return (
-              <th scope="col" key={heading}>
-                {heading}
-              </th>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody>{renderTable(props.data)}</tbody>
+      <table className="table table-striped">
+        <thead>
+          <tr>{renderHeading(newTable.head)}</tr>
+        </thead>
+        <tbody>{renderBody(newTable.body)}</tbody>
       </table>
-      </div>
+    </div>
   );
 }
 

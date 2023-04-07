78e13788bede7f4589f4cb834c9ad65181249183
Pramath S.B <pramathsb@gmail.com>
2023-04-07 21:58:10 +0530

    update

--- src/code/components/VehicleInfo/FuelTable.js
+++ src/code/components/VehicleInfo/FuelTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function FuelTable(props) {
-  const { fuelData, TXT } = props;
+  const { modifiedfuelData, TXT } = props;
   return (
     <table className='table table-bordered'>
       <thead>
@@ -12,23 +12,47 @@ function FuelTable(props) {
           <th>{TXT.dashboard.table.quantity}</th>
           <th>{TXT.dashboard.table.totalPrice}</th>
           <th>{TXT.dashboard.table.fullTank}</th>
-          <th>{TXT.dashboard.table.distance}</th>
+          <th>{TXT.dashboard.table.trip}</th>
           <th>{TXT.dashboard.table.mileage}</th>
         </tr>
       </thead>
       <tbody>
-        {fuelData.map((row) => {
-          const { date, odo, fuelPrice, qtyLtrs, qtyRs, fullTank } = row;
+        {modifiedfuelData.map((row, i) => {
+          const { date, odo, fuelPrice, qtyLtrs, qtyRs, fullTank, trip, mileage } = row;
           return (
             <tr key={odo}>
               <td>{date}</td>
               <td>{odo}</td>
               <td>{fuelPrice}</td>
-              <td>{qtyRs}</td>
               <td>{qtyLtrs}</td>
-              <td>{fullTank}</td>
-              <td>distance</td>
-              <td>mileage</td>
+              <td>{qtyRs}</td>
+              <td>
+                {fullTank ? (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='32'
+                    fill='green'
+                    class='bi bi-battery-full'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M2 6h10v4H2V6z' />
+                    <path d='M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z' />
+                  </svg>
+                ) : (
+                  <svg
+                    width='32'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='red'
+                    class='bi bi-battery-half'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M2 6h5v4H2V6z' />
+                    <path d='M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z' />
+                  </svg>
+                )}
+              </td>
+              <td>{trip}</td>
+              <td>{mileage}</td>
             </tr>
           );
         })}
--- src/code/components/VehicleInfo/index.js
+++ src/code/components/VehicleInfo/index.js
@@ -11,6 +11,46 @@ function VehicleInfo(props) {
   const { dashBoardData, fuelData } = vehicle;
   const { TXT } = store;
 
+  const dashboard = {
+    totalFuel: 0,
+    odo: 0,
+    totalFuelPrice: 0,
+    minFullTankDistance: 1000,
+    maxFullTankDistance: 0,
+    avgFullTankDistance: [],
+  };
+
+  const modifiedfuelData =
+    fuelData &&
+    fuelData.map((data, i) => {
+      // Distance
+      const curOdo = fuelData[i].odo;
+      const prevOdo = () => fuelData[i - 1].odo;
+      const trip = i > 0 ? curOdo - prevOdo() : 'NA';
+
+      // Mileage
+      const prevFulltank = () => fuelData[i - 1].fullTank;
+      const curFulltank = () => fuelData[i].fullTank;
+      const prevFuelFillQty = () => fuelData[i - 1].qtyLtrs;
+      const mileage = i > 0 && prevFulltank() ? (trip / prevFuelFillQty()).toFixed(2) : 'NA';
+
+      // Odo
+      dashboard.odo = curOdo;
+      dashboard.totalFuel += data.qtyLtrs;
+      dashboard.totalFuelPrice += data.qtyRs;
+      dashboard.minFullTankDistance =
+        i > 0 && prevFulltank() && curFulltank() && trip < dashboard.minFullTankDistance
+          ? trip
+          : dashboard.minFullTankDistance;
+      dashboard.maxFullTankDistance =
+        i > 0 && prevFulltank() && curFulltank() && trip > dashboard.maxFullTankDistance
+          ? trip
+          : dashboard.maxFullTankDistance;
+      i > 0 && prevFulltank() && curFulltank() && dashboard.avgFullTankDistance.push(trip);
+
+      return { ...data, trip, mileage };
+    });
+
   const renderDashboard = () => {
     return (
       <div className='row flex-wrap g-0 dashboard p-4 mb-4'>
@@ -48,31 +88,35 @@ function VehicleInfo(props) {
           {TXT.dashboard.nextServiceKm}: {dashBoardData.nextServiceKm}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.distanceTraveled}: {dashBoardData.distanceTraveled}
+          {TXT.dashboard.distanceTraveled}: {dashboard.odo}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.totalFuelConsumedLtrs}: {dashBoardData.totalFuelConsumedLtrs}
+          {TXT.dashboard.totalFuelConsumedLtrs}: {dashboard.totalFuel.toFixed(2)}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.totalFuelConsumedCost}: {dashBoardData.totalFuelConsumedCost}
+          {TXT.dashboard.totalFuelConsumedCost}: {dashboard.totalFuelPrice.toFixed(2)}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.totalAverageMileage}: {dashBoardData.totalAverageMileage}
+          {TXT.dashboard.totalAverageMileage}: {(dashboard.odo / dashboard.totalFuel).toFixed(2)}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.costPerKm}: {dashBoardData.costPerKm}
+          {TXT.dashboard.costPerKm}: {(dashboard.totalFuelPrice / dashboard.odo).toFixed(2)}
         </div>
         <div className='col-4 p-2'>
           {TXT.dashboard.model}: {dashBoardData.model}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.minDistOnFullTank}: {dashBoardData.minDistOnFullTank}
+          {TXT.dashboard.minDistOnFullTank}: {dashboard.minFullTankDistance}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.maxDistOnFullTank}: {dashBoardData.maxDistOnFullTank}
+          {TXT.dashboard.maxDistOnFullTank}: {dashboard.maxFullTankDistance}
         </div>
         <div className='col-4 p-2'>
-          {TXT.dashboard.avgDistOnFullTank}: {dashBoardData.avgDistOnFullTank}
+          {TXT.dashboard.avgDistOnFullTank}:{' '}
+          {(
+            dashboard.avgFullTankDistance.reduce((prev, curr) => prev + curr) /
+            dashboard.avgFullTankDistance.length
+          ).toFixed(2)}
         </div>
       </div>
     );
@@ -88,7 +132,7 @@ function VehicleInfo(props) {
       {
         route: 'fuel',
         tabName: 'Fuel',
-        component: fuelData && <FuelTable fuelData={fuelData} TXT={TXT} />,
+        component: fuelData && <FuelTable modifiedfuelData={modifiedfuelData} TXT={TXT} />,
       },
       {
         route: 'service',
